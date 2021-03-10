10c6beb9d0bf9f88c8a389f15c1e31ba10e6922f
Gr4vity4 <prawared.bw@gmail.com>
2021-03-10 11:58:09 +0700

    update

--- index.js
+++ index.js
@@ -4,9 +4,7 @@
  */
 const express = require("express");
 const app = express();
-const path = require("path");
 const PORT = process.env.PORT || 9999;
-const request = require("request");
 const bodyParser = require("body-parser");
 
 /*
